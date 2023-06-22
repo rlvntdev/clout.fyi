98182076bebe96f1759327434155d269ef60ee1a
Derek <dereknelsony0@gmail.com>
2023-06-22 13:46:29 -0400

    landing

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -2,6 +2,12 @@ import { Anchor, Button, H1, Paragraph, Separator, Sheet, useToastController, XS
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
+import {
+	SigmaContainer,
+	useRegisterEvents,
+	useLoadGraph,
+	useSigmaContext,
+  } from "@react-sigma/core";
 
 export function HomeScreen() {
 	const linkProps = useLink({
@@ -11,29 +17,12 @@ export function HomeScreen() {
 	return (
 		<YStack f={1} jc="center" ai="center" p="$4" space>
 			<YStack space="$4" maw={600}>
-				<H1 ta="center">Welcome to Tamagui.</H1>
+				<H1 ta="center">welcome to clout.fyi</H1>
 				<Paragraph ta="center">
-					Here's a basic starter to show navigating from one screen to another. This screen uses the same code on Next.js and React Native.
+					this is where the accloutant lives. more soon!
 				</Paragraph>
 
-				<Separator />
-				<Paragraph ta="center">
-					Made by{' '}
-					<Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-						@natebirdman
-					</Anchor>
-					,{' '}
-					<Anchor color="$color12" href="https://github.com/tamagui/tamagui" target="_blank" rel="noreferrer">
-						give it a ⭐️
-					</Anchor>
-				</Paragraph>
 			</YStack>
-
-			<XStack>
-				<Button {...linkProps}>Link to user</Button>
-			</XStack>
-
-			<SheetDemo />
 		</YStack>
 	)
 }
